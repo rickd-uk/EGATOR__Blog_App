ec701b33f142f98ca076ce08680939408808dfb2
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-08-02 18:59:35 +0900

    add: display posts on home page

--- index.php
+++ index.php
@@ -1,209 +1,126 @@
-<?php include 'partials/header.php' ?>
+<?php include 'partials/header.php';
+
+// fetch featured posts from db
+$query = "SELECT * FROM posts WHERE is_featured=1";
+$result = mysqli_query($con, $query);
+$featured = mysqli_fetch_assoc($result);
+
+// Fetch posts
+$posts_query = "SELECT * FROM posts ORDER BY date_time DESC LIMIT 9";
+$posts = mysqli_query($con, $posts_query)
+?>
 
 <!-- #region FEATURED POST -->
-<section class="featured">
-  <div class="container featured__container">
-    <div class="post__thumbnail">
-      <img src="./images/blog1.jpg" />
-    </div>
-    <div class="post__info">
-      <a href="" class="category__button">Wildlife</a>
-      <h2 class="post__title"><a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, nostrum?</a></h2>
-      <p class="post__body">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi temporibus aliquid sit nisi odit commodi asperiores voluptate
-        qui impedit minus accusamus id nihil beatae eius, explicabo error quibusdam odio nam.
-      </p>
-      <div class="post__author">
-        <div class="post__author-avatar"><img src="./images/avatar2.jpg" alt="" /></div>
-        <div class="post__author-info">
-          <h5>By: Jane Doe</h5>
-          <small>June 10, 2022 - 07:34</small>
+
+<?php if (mysqli_num_rows($result) == 1) : ?>
+  <section class="featured">
+    <div class="container featured__container">
+      <div class="post__thumbnail" style="width: 400px;">
+        <img src="./images/posts/<?= $featured['thumbnail'] ?>" />
+      </div>
+      <div class="post__info">
+
+        <?php // fetch category from categories using category_id
+        $category_id = $featured['category_id'];
+        $category_query = "SELECT * FROM categories WHERE id=$category_id";
+        $category_result = mysqli_query($con, $category_query);
+        $category = mysqli_fetch_assoc($category_result);
+
+        ?>
+        <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $featured['category_id'] ?>" class="category__button">
+          <?= $category['title'] ?>
+        </a>
+        <h2 class="post__title"><a href="<?= ROOT_URL ?>post.php?id=<?= $featured['id'] ?>"><?= $featured['title'] ?></a></h2>
+        <p class="post__body">
+          <?= substr($featured['body'], 300) ?>...
+        </p>
+        <div class="post__author">
+          <?php
+          // Fetch author from users table using author id
+          $author_id = $featured['author_id'];
+          $author_query = "SELECT * FROM users WHERE id=$author_id";
+          $author_result = mysqli_query($con, $author_query);
+          $author = mysqli_fetch_assoc($author_result);
+          $author_firstname = $author['firstname'];
+          $author_lastname = $author['lastname'];
+          ?>
+          <div class="post__author-avatar"><img src="./images/users/<?= $author['avatar'] ?>" alt="" /></div>
+          <div class="post__author-info">
+            <h5>By: <?= "{$author_firstname} {$author_lastname}" ?></h5>
+            <small>
+              <?= date("M d, Y - H:i", strtotime($featured['date_time'])) ?>
+            </small>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
 <!-- #endregion FEATURED POST -->
 
 <!-- #region POSTS -->
 <section class="posts">
   <div class="container posts__container">
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog2.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar3.jpg" alt="" />
+    <?php while ($post = mysqli_fetch_assoc($posts)) : ?>
+      <article class="post">
+        <div class="post__thumbnail" style="width: 300px; height: 200px;">
+          <img src="./images/posts/<?= $post['thumbnail'] ?>" />
+        </div>
+        <div class="post__info">
+          <?php // fetch category from categories using category_id
+          $category_id = $post['category_id'];
+          $category_query = "SELECT * FROM categories WHERE id=$category_id";
+          $category_result = mysqli_query($con, $category_query);
+          $category = mysqli_fetch_assoc($category_result);
+
+          ?>
+          <a href="<?= ROOT_URL ?>category-posts?id=<?= $post['category_id'] ?>" class="category__button"><?= $category['title'] ?></a>
+          <h3 class="post__title">
+            <a href="<?= ROOT_URL ?>post.php"><?= $post['title'] ?></a>
+            <p class="post__body" style="min-height: 100px;">
+              <?= substr($post['body'], 0, 120) ?>...
+            </p>
+            <div class="post__author">
+              <?php
+              // Fetch author from users table using author id
+              $author_id = $post['author_id'];
+              $author_query = "SELECT * FROM users WHERE id=$author_id";
+              $author_result = mysqli_query($con, $author_query);
+              $author = mysqli_fetch_assoc($author_result);
+              $author_firstname = $author['firstname'];
+              $author_lastname = $author['lastname'];
+              ?>
+              <div class="post__author-avatar">
+                <img src="./images/users/<?= $author['avatar'] ?>" alt="" />
+              </div>
+              <div class="post__author-info">
+                <h5>By: <?= "{$author_firstname} {$author_lastname}" ?></h5>
+                <small><?= date("M d, Y - H:i", strtotime($post['date_time'])) ?></small>
+              </div>
             </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog3.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar2.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog4.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar4.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog5.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar5.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog6.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar4.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog7.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar6.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
-    <article class="post">
-      <div class="post__thumbnail">
-        <img src="./images/blog8.jpg" />
-      </div>
-      <div class="post__info">
-        <a href="" class="category__button">Wildlife</a>
-        <h3 class="post__title">
-          <a href="<?= ROOT_URL ?>post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-          <p class="post__body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non vitae blanditiis in ipsam reprehenderit incidunt dolor
-          </p>
-          <div class="post__author">
-            <div class="post__author-avatar">
-              <img src="./images/avatar7.jpg" alt="" />
-            </div>
-            <div class="post__author-info">
-              <h5>By: John Mills</h5>
-              <small>June 13, 2022 - 12:30</small>
-            </div>
-          </div>
-        </h3>
-      </div>
-    </article>
+          </h3>
+        </div>
+      </article>
+    <?php endwhile; ?>
   </div>
 </section>
 <!-- #endregion POSTS -->
 
+
+
 <!-- #region CATEGORY BUTTONS -->
 <section class="category__buttons">
   <div class="container category__buttons-container">
-    <a href="" class="category__button">Art</a>
-    <a href="" class="category__button">Wildlife</a>
-    <a href="" class="category__button">Travel</a>
-    <a href="" class="category__button">Science</a>
-    <a href="" class="category__button">Food</a>
-    <a href="" class="category__button">Music</a>
+    <?php
+    $all_categories_query = "SELECT * FROM categories";
+    $all_categories = mysqli_query($con, $all_categories_query);
+    ?>
+    <?php while ($category = mysqli_fetch_assoc($all_categories)) : ?>
+      <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $category['id'] ?>" class="category__button"><?= $category['title'] ?></a>
+    <?php endwhile; ?>
+
   </div>
 </section>
 <!-- #endregion CATEGORY BUTTONS -->
 
-<?php include 'partials/footer.php';
+<?php include 'partials/footer.php'; ?>
